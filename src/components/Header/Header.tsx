a0bd1cb2a4f26703dd538fcc308569102098f95e
Anton Chekalenko <aston23112001@gmail.com>
2023-09-03 21:00:31 +0300

    first commit on branch main

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import './style.module.css'
+
+export default function Header() {
+    
+
+  return (
+    <section className='content__container header__container'>
+      <LogoDevIcon/>
+      
+    </section>
+  )
+}
---
+++ src/components/Header/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li className='login'><a href="#">Log in</a></li>
+            <li className='logout'><a href="#">Log Out</a></li>
+        </ul>      
+    </nav>
+  )
+}
