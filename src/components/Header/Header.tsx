9b98354dfc159a8703980b6e7367b0564550a7b8
Anton Chekalenko <aston23112001@gmail.com>
2023-09-03 22:46:37 +0300

    created header tsx, added dependencys

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
 import React, { FC } from 'react'
 import LogoDevIcon from '@mui/icons-material/LogoDev';
-import './style.module.css'
+import './style.css'
+import Nav from './nav/Nav';
+import { Container } from '@mui/material';
 
 export default function Header() {
     
 
   return (
-    <section className='content__container header__container'>
-      <LogoDevIcon/>
-      
+    <section className='header'>
+      <Container maxWidth="lg" className='header__container'>
+        <LogoDevIcon />
+        <Nav />
+      </Container>
     </section>
+      
   )
 }
--- src/components/Header/nav/Nav.tsx
+++ src/components/Header/nav/Nav.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 
 export default function Nav() {
   return (
     <nav>
         <ul>
-            <li className='login'><a href="#">Log in</a></li>
+            <li className='login'><a href="#"><Button variant="contained">Hello world</Button></a></li>
             <li className='logout'><a href="#">Log Out</a></li>
         </ul>      
     </nav>
